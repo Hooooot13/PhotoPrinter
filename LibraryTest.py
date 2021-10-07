89cb1d0fb1ff1b5ed1ff509bbe96eccacc64d994
Hooooot13 <lcdx5000@gmail.com>
2021-10-07 17:56:04 -0500

    commit

--- LibraryTest.py
+++ LibraryTest.py
@@ -1,5 +1,22 @@
 from PhotoPrintingLibrary import photo
+import os
 
-Photo = photo("Exports/DSCF0691-Edit.jpg", "The Temple")
-Photo.print()
-Photo.save()
+
+#A Boy and His Camera
+Photo1 = photo(f"PXL_20210922_044429634_Original.jpg", "A Boy and His Camera")
+Photo1.reProcess(3000, 1, 2)
+Photo1.save()
+
+#Power Lines
+Photo2 = photo(f"PXL_20210922_044429634_Original.jpg", "A Boy and His Camera")
+Photo2.save()
+
+#Byodo-In Temple
+Photo3 = photo(f"PXL_20210922_044429634_Original.jpg", "A Boy and His Camera")
+Photo3.save()
+
+#Nap Time
+Photo4 = photo(f"PXL_20210922_044429634_Original.jpg", "A Boy and His Camera")
+Photo4.save()
+
+
--- PhotoPrintingLibrary.py
+++ PhotoPrintingLibrary.py
@@ -5,7 +5,7 @@ import math
 import os
 
 class photo:
-    def __init__(self, path, title="Unamed Photo"):
+    def __init__(self, path, title=""):
         #Read in image
         self.fileName = path.split('/')[-1]
         self.image = cv2.imread(path)
@@ -18,12 +18,20 @@ class photo:
         self.make = self.exifdata.get(271)
         self.model = self.exifdata.get(272)
         self.focalLegth = self.exifdata.get(37386)
-        self.shutterSpeed = math.ceil(1/float(self.exifdata.get(0x829a)))
-        self.F = f"F/{int(self.exifdata.get(37378))}"
+        self.shutterSpeed = float(self.exifdata.get(0x829a))
+
+        if self.shutterSpeed >= 1.0:
+            self.shutterSpeed = self.exifdata.get(0x829a)
+            SSprint = f"{int(self.exifdata.get(0x829a))}"
+        else:
+            self.shutterSpeed = math.ceil(1/float(self.exifdata.get(0x829a)))
+            SSprint = f"1/{self.shutterSpeed}"
+
+        self.F = f"F/{round(float(self.exifdata.get(37378)), 1)}"
         self.ISO = self.exifdata.get(34855)
 
         #Generate text
-        self.text = f"{self.make} {self.model} | {self.focalLegth} mm | f/{self.F} | SS 1/{self.shutterSpeed} | ISO {self.ISO}"
+        self.text = f"{self.make} {self.model} | {self.focalLegth} mm | {self.F} | SS {SSprint} | ISO {self.ISO}"
 
         #Calcualte Border
         if self.image.shape[1] >= self.image.shape[0]:
@@ -82,5 +90,33 @@ class photo:
         #closing all open windows
         cv2.destroyAllWindows()
 
+    def reProcess(self, newSize, fontsize, linewidth):
+        self.squareSize = newSize
+        self.top = int((self.squareSize-self.image.shape[0])/2)
+        self.bottom = int((self.squareSize-self.image.shape[0])/2)
+        self.left = int((self.squareSize-self.image.shape[1])/2)
+        self.right = int((self.squareSize-self.image.shape[1])/2)
+        self.ImageBottom = self.top + self.image.shape[0]
+
+        self.image_with_border = cv2.copyMakeBorder(self.image, self.top, self.bottom, self.left, self.right, cv2.BORDER_CONSTANT, value=self.borderColor)
+
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.fontsize = fontsize
+        self.linewidth = linewidth
+        self.textsize = cv2.getTextSize(self.text, self.font, self.fontsize, self.linewidth )[0]
+        self.textColor = (0, 0, 0)
+
+        textX = int((self.image_with_border.shape[1] - self.textsize[0]) / 2)
+        textY = self.ImageBottom + self.textsize[1] + 30  #int((img.shape[0] + textsize[1]) / 2)
+        self.imageWithText = cv2.putText(self.image_with_border, self.text, ( textX, textY ), self.font, self.fontsize, self.textColor, self.linewidth)
+
+        self.titlesize = cv2.getTextSize(self.title, self.font, self.fontsize, self.linewidth )[0]
+        textX = int((self.image_with_border.shape[1] - self.titlesize[0]) / 2)
+        textY = self.top - self.titlesize[1] - 30
+        self.imageWithText = cv2.putText(self.imageWithText, self.title, ( textX, textY ), self.font, self.fontsize, self.textColor, self.linewidth)
+
+        self.resizedImage = cv2.resize(self.imageWithText, self.finalResolution, interpolation = cv2.INTER_AREA)
+    
+    
     def save(self):
         cv2.imwrite(f"{self.fileName.split('.')[0]}_B.jpg", self.imageWithText)
